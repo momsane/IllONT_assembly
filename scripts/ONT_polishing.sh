be3507b1c4aa4b0072a9fe64437f5149286ffede
momsane <146840224+momsane@users.noreply.github.com>
2024-09-28 21:08:09 +0200

    finished ONT assembly and polishing with ONT reads; tried to use the lastest version of graphmap (graphmap2) but switched to version 0.5.2 because the new does not work; started fixing the Illumina polishing

--- scripts/ONT_polishing.sh
+++ scripts/ONT_polishing.sh
@@ -1,55 +1,53 @@
+#!/bin/bash
+
 ###===========================
 ##Priming the variables used
-echo -e "-------0. Priming the variables used"
+echo -e "---0. Priming the variables used"
 ###===========================
+sample_name=$1
 ont_reads=$2
 reference_fasta=$3
-sample_name=$1
-
-Overall_output_directory=$4
+outdir=$4
 
 overall_polishing_counter=1
+
 ###===========================
 ##Starting the for-loop
-echo -e "-------0. Starting the for-loop"
+echo -e "\n-----0. Starting the for-loop"
 ###===========================
 
 for ONT_polishing_counter in $(echo "first second"); do
-  echo -e "----"${ONT_polishing_counter}" round of Racon polishing"
-
+  echo -e "\n-------"${ONT_polishing_counter}" round of Racon polishing"
   ###===========================
   ##graphmap mapping
-  echo -e "-------1. First we map with graphmap"
+  echo -e "--------1. First we map with graphmap"
   ###===========================
 
-  mkdir -p $Overall_output_directory/0${overall_polishing_counter}_${ONT_polishing_counter}_Racon_graphmap/{Graphmap_ONT,assembly}
-
-  graphmap align --rebuild-index --circular  \
-      -r $reference_fasta \
-      -d $ont_reads -o ${Overall_output_directory}/0${overall_polishing_counter}_${ONT_polishing_counter}_Racon_graphmap/Graphmap_ONT/${ONT_polishing_counter}_Racon_ONT2FinalAssembly_sorted.sam
-
+  mkdir -p $outdir/0${overall_polishing_counter}_${ONT_polishing_counter}_Racon_graphmap/{Graphmap_ONT,assembly}
+  
+  # Store path to output sam into a variable
+  overlap=${outdir}/0${overall_polishing_counter}_${ONT_polishing_counter}_Racon_graphmap/Graphmap_ONT/${ONT_polishing_counter}_Racon_ONT2FinalAssembly_sorted.sam
+  
+  graphmap align --rebuild-index --circular -r ${reference_fasta} -d ${ont_reads} -o ${overlap}
+  
   ###===========================
   ##racon polishing
-  echo -e "-------2. Second polish with Racon"
+  echo -e "--------2. Then we polish with Racon"
   ###===========================
 
-
-  touch ${Overall_output_directory}/0${overall_polishing_counter}_${ONT_polishing_counter}_Racon_graphmap/assembly/${sample_name}_${ONT_polishing_counter}_Racon_Assembly.fasta
-  echo -e "####### REF ${reference_fasta} ###################"
-  echo -e "####### Reads ${ont_reads} ###################"
-  echo -e "####### OV ${Overall_output_directory}/0${overall_polishing_counter}_${ONT_polishing_counter}_Racon_graphmap/Graphmap_ONT/${ONT_polishing_counter}_Racon_ONT2FinalAssembly_sorted.sam ###################"
-  racon ${ont_reads} \
-  ${Overall_output_directory}/0${overall_polishing_counter}_${ONT_polishing_counter}_Racon_graphmap/Graphmap_ONT/${ONT_polishing_counter}_Racon_ONT2FinalAssembly_sorted.sam \
-  ${reference_fasta} > \
-  ${Overall_output_directory}/0${overall_polishing_counter}_${ONT_polishing_counter}_Racon_graphmap/assembly/${sample_name}_${ONT_polishing_counter}_Racon_Assembly.fasta
-
+  echo -e "Reference assembly:  " ${reference_fasta} "  of size:" $(stat --printf="%s" ${reference_fasta}) "bytes"
+  echo -e "Reads for correction:  " ${ont_reads} "  of size:" $(stat --printf="%s" ${ont_reads}) "bytes"
+  echo -e "Overlap set:  " ${overlap} "  of size:" $(stat --printf="%s" ${overlap}) "bytes"
+  
+  racon ${ont_reads} ${overlap} ${reference_fasta} > \
+  ${outdir}/0${overall_polishing_counter}_${ONT_polishing_counter}_Racon_graphmap/assembly/${sample_name}_${ONT_polishing_counter}_Racon_Assembly.fasta
 
   ###===========================
   ##Reset the variables and reference
-  echo -e "-------3. Reset the variables and reference"
+  echo -e "--------3. Reset the variables and reference assembly"
   ###===========================
 
-  reference_fasta=$(echo ${Overall_output_directory}/0${overall_polishing_counter}_${ONT_polishing_counter}_Racon_graphmap/assembly/${sample_name}_${ONT_polishing_counter}_Racon_Assembly.fasta)
+  reference_fasta=${outdir}/0${overall_polishing_counter}_${ONT_polishing_counter}_Racon_graphmap/assembly/${sample_name}_${ONT_polishing_counter}_Racon_Assembly.fasta
   overall_polishing_counter=$((overall_polishing_counter+1))
 
 done
--- scripts/illumina_polishing.sh
+++ scripts/illumina_polishing.sh
@@ -1,32 +1,37 @@
-module load gcc
-module load bowtie2
-module load samtools
-module load pilon
-# This script doesn't wok in a conda env for some reason, so I run it using the cluster softwares
+#!/bin/bash
+
+# module purge
+
+# module load gcc
+# module load bowtie2/2.5.1
+# module load samtools/1.17
+# module load pilon
+# # This script doesn't work in a conda env for some reason, so I run it using the cluster softwares
 
 threads=$1
-Overall_output_directory=$5
 reference_fasta=$2
 R1=$3
 R2=$4
+outdir=$5
 sample_name=$6
 
 ###===========================
 ##Priming the variables used and starting the for-loop
-echo -e "-------0. Priming the variables used and starting the for-loop"
+echo -e "-----0. Priming the variables used and starting the for-loop"
 ###===========================
 
 overall_polishing_counter=3
 
 for Illumina_polishing_counter in $(echo "first second third"); do
 
-  echo -e "----"${Illumina_polishing_counter}" round of pilon polishing"
+  echo -e "\n-------"${Illumina_polishing_counter}" round of pilon polishing"
 
   ###===========================
   ##bowtie2 mapping
-  echo -e "-------1. First we map with bowtie2"
+  echo -e "---------1. First we map with bowtie2"
   ###===========================
-  mkdir -p ${Overall_output_directory}/0${overall_polishing_counter}_${Illumina_polishing_counter}_pillon_bowtie2/{bowtie2,assembly}
+  mkdir -p ${outdir}/0${overall_polishing_counter}_${Illumina_polishing_counter}_pilon_bowtie2/{bowtie2,assembly}
+  bam=${outdir}/0${overall_polishing_counter}_${Illumina_polishing_counter}_pilon_bowtie2/bowtie2/${Illumina_polishing_counter}_pilon_ONT2FinalAssembly_sorted.bam
 
   bowtie2-build --quiet ${reference_fasta} ${reference_fasta}
 
@@ -34,33 +39,33 @@ for Illumina_polishing_counter in $(echo "first second third"); do
           -2 ${R2} \
           -x  ${reference_fasta} \
           --threads ${threads} \
-          --local --very-sensitive-local | samtools sort -O BAM -o ${Overall_output_directory}/0${overall_polishing_counter}_${Illumina_polishing_counter}_pillon_bowtie2/bowtie2/${Illumina_polishing_counter}_pillon_ONT2FinalAssembly_sorted.bam -
+          --local --very-sensitive-local | samtools sort -O BAM -o ${bam}
 
   ###--------------------
   ##index bam file
   ###--------------------
-  samtools index ${Overall_output_directory}/0${overall_polishing_counter}_${Illumina_polishing_counter}_pillon_bowtie2/bowtie2/${Illumina_polishing_counter}_pillon_ONT2FinalAssembly_sorted.bam
+  samtools index ${bam}
 
   ###===========================
   ##pilon polishing
-  echo -e "-------2. Second polish with pilon"
+  echo -e "---------2. Then polish with pilon"
   ###===========================
-  echo -e "${Overall_output_directory}/0${overall_polishing_counter}_${Illumina_polishing_counter}_pillon_bowtie2/assembly/"
+  echo -e "${outdir}/0${overall_polishing_counter}_${Illumina_polishing_counter}_pilon_bowtie2/assembly/"
 
   pilon --genome ${reference_fasta} \
-   --frags ${Overall_output_directory}/0${overall_polishing_counter}_${Illumina_polishing_counter}_pillon_bowtie2/bowtie2/${Illumina_polishing_counter}_pillon_ONT2FinalAssembly_sorted.bam \
-   --output ${sample_name}_${Illumina_polishing_counter}_Pilon_Assembly \
-   --outdir ${Overall_output_directory}/0${overall_polishing_counter}_${Illumina_polishing_counter}_pillon_bowtie2/assembly/ \
-   --changes
+  --frags ${bam} \
+  --output ${sample_name}_${Illumina_polishing_counter}_Pilon_Assembly \
+  --outdir ${outdir}/0${overall_polishing_counter}_${Illumina_polishing_counter}_pilon_bowtie2/assembly/ \
+  --changes
 
-  tree ${Overall_output_directory}/0${overall_polishing_counter}_${Illumina_polishing_counter}_pillon_bowtie2/
+  tree ${outdir}/0${overall_polishing_counter}_${Illumina_polishing_counter}_pilon_bowtie2/
 
   ###===========================
   ##reset the parameters
-  echo -e "-------3. Resetting the parameters"
+  echo -e "---------3. Resetting the parameters"
   ###===========================
 
-  reference_fasta=$(echo ${Overall_output_directory}/0${overall_polishing_counter}_${Illumina_polishing_counter}_pillon_bowtie2/assembly/${sample_name}_${Illumina_polishing_counter}_Pilon_Assembly.fasta)
+  reference_fasta=$(echo ${outdir}/0${overall_polishing_counter}_${Illumina_polishing_counter}_pilon_bowtie2/assembly/${sample_name}_${Illumina_polishing_counter}_Pilon_Assembly.fasta)
   echo ${reference_fasta}
   overall_polishing_counter=$((overall_polishing_counter+1))
 
