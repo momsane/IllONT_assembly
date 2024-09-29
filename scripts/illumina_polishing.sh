959392bff02e9278990e0ff2f483c05bc824f160
momsane <146840224+momsane@users.noreply.github.com>
2024-09-29 18:05:04 +0200

    successfully ran the illumina polishing step using conda envs on one sample

--- scripts/illumina_polishing.sh
+++ scripts/illumina_polishing.sh
@@ -24,20 +24,23 @@ overall_polishing_counter=3
 
 for Illumina_polishing_counter in $(echo "first second third"); do
 
-  echo -e "\n-------"${Illumina_polishing_counter}" round of pilon polishing"
+  echo -e "\n\n\n-------"${Illumina_polishing_counter}" round of pilon polishing"
 
   ###===========================
   ##bowtie2 mapping
   echo -e "---------1. First we map with bowtie2"
   ###===========================
-  mkdir -p ${outdir}/0${overall_polishing_counter}_${Illumina_polishing_counter}_pilon_bowtie2/{bowtie2,assembly}
-  bam=${outdir}/0${overall_polishing_counter}_${Illumina_polishing_counter}_pilon_bowtie2/bowtie2/${Illumina_polishing_counter}_pilon_ONT2FinalAssembly_sorted.bam
+  root=${outdir}/0${overall_polishing_counter}_${Illumina_polishing_counter}_pilon_bowtie2
+  
+  mkdir -p ${root}/{bowtie2,assembly}
+  
+  bam=${root}/bowtie2/${Illumina_polishing_counter}_pilon_ONT2FinalAssembly_sorted.bam
 
-  bowtie2-build --quiet ${reference_fasta} ${reference_fasta}
+  bowtie2-build --quiet ${reference_fasta} ${reference_fasta%.fasta}
 
   bowtie2 -1 ${R1} \
           -2 ${R2} \
-          -x  ${reference_fasta} \
+          -x ${reference_fasta%.fasta} \
           --threads ${threads} \
           --local --very-sensitive-local | samtools sort -O BAM -o ${bam}
 
@@ -49,25 +52,23 @@ for Illumina_polishing_counter in $(echo "first second third"); do
   ###===========================
   ##pilon polishing
   echo -e "---------2. Then polish with pilon"
+  echo -e "Output directory:" "${root}/assembly/"
   ###===========================
-  echo -e "${outdir}/0${overall_polishing_counter}_${Illumina_polishing_counter}_pilon_bowtie2/assembly/"
 
   pilon --genome ${reference_fasta} \
   --frags ${bam} \
   --output ${sample_name}_${Illumina_polishing_counter}_Pilon_Assembly \
-  --outdir ${outdir}/0${overall_polishing_counter}_${Illumina_polishing_counter}_pilon_bowtie2/assembly/ \
+  --outdir ${root}/assembly/ \
   --changes
 
-  tree ${outdir}/0${overall_polishing_counter}_${Illumina_polishing_counter}_pilon_bowtie2/
+  tree ${root}
 
   ###===========================
   ##reset the parameters
   echo -e "---------3. Resetting the parameters"
   ###===========================
 
-  reference_fasta=$(echo ${outdir}/0${overall_polishing_counter}_${Illumina_polishing_counter}_pilon_bowtie2/assembly/${sample_name}_${Illumina_polishing_counter}_Pilon_Assembly.fasta)
-  echo ${reference_fasta}
+  reference_fasta=${root}/assembly/${sample_name}_${Illumina_polishing_counter}_Pilon_Assembly.fasta
   overall_polishing_counter=$((overall_polishing_counter+1))
 
 done
-#polishing round
